ba6f86313f8e18a7d09b2293454d420cd0ad648f
Fola <jimiakinwale@icloud.com>
2022-05-03 15:36:37 +0100

    fixed eslint issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import VotePage from "./components/Votepage";
 import LeaderboardPage from "./components/LeaderboardPage";
 import Header from "./components/Header";
