4ecc5fa2a8b3621b73db52a3385dabb68f661689
Fola <jimiakinwale@icloud.com>
2022-05-03 15:30:50 +0100

    added react router to app and added leaderboard,header and vote page to component file

---
+++ src/components/LeaderboardPage.tsx
@@ -0,0 +1,4 @@
+
+export default function LeaderboardPage ():JSX.Element{
+    return (<h1>Leaderboard Page</h1>)
+}
---
+++ src/components/Votepage.tsx
@@ -0,0 +1,4 @@
+
+export default function VotePage ():JSX.Element{
+    return (<h1>Vote page</h1>)
+}
