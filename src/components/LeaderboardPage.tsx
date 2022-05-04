2bd166c7de89390669e112111d15278691ed9f48
Soso Ogan <soso737@hotmail.co.uk>
2022-05-04 13:08:01 +0100

    created leaderboard contents

--- src/components/LeaderboardPage.tsx
+++ src/components/LeaderboardPage.tsx
@@ -1,3 +1,36 @@
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import {dogInfo} from '../utils/interfaces'
 export default function LeaderboardPage(): JSX.Element {
-  return <h1>Leaderboard Page</h1>;
+
+const [leaderboard, setLeaderboard] = useState<dogInfo[]>([])
+console.log(leaderboard)
+useEffect(() => {
+  axios
+    .get("https://dog-breed-voting.herokuapp.com/")
+    .then((response) => {
+      console.log("getting all entries: ", response.data);
+      const receivedtopTenDogInfo = response.data;
+      console.log(receivedtopTenDogInfo)
+      setLeaderboard(receivedtopTenDogInfo);
+    })
+    .catch((err) => console.error("error when getting entries", err));
+}, []);
+
+function getOneTopDogInfo(dogProfile: dogInfo, id: number) {
+ console.log(dogProfile.sumvote_count)
+  return <li className ='OneListItem' key= {id}>{dogProfile.breed} {parseInt(dogProfile.sumvote_count) === 1 ? `${parseInt(dogProfile.sumvote_count)} vote`:`${parseInt(dogProfile.sumvote_count)} votes`}</li>
+}
+  return(
+
+  <>
+    <h1>Leaderboard Page</h1>
+
+    <ol className = 'ListOfTen'>
+      {leaderboard.map(getOneTopDogInfo)}
+    </ol>
+  </>
+  
+  
+  )
 }
