5f5b5a4065e0b6147ccbfc9cac4972213e2a6ad4
Soso Ogan <soso737@hotmail.co.uk>
2022-05-05 16:41:54 +0100

    added yarn format to cypress tests

--- src/components/Votepage.tsx
+++ src/components/Votepage.tsx
@@ -79,7 +79,7 @@ export default function VotePage(): JSX.Element {
       <div className="flex-row-container">
         <div>
           <img
-            style = {{width: "200px"}}
+            style={{ width: "200px" }}
             src={images[0] ? images[0].message : ""}
             alt="placeholder"
             onClick={() => (firstDog ? updateVote(firstDog.id) : 0)}
@@ -91,7 +91,7 @@ export default function VotePage(): JSX.Element {
 
         <div>
           <img
-            style = {{width: "200px"}}
+            style={{ width: "200px" }}
             src={images[1] ? images[1].message : ""}
             alt="placeholder"
             onClick={() => (secondDog ? updateVote(secondDog.id) : 0)}
